ae6fd96fa44e8369640b39679a771a974b7d29fa
Yusuke Yagyu (gyugyu) <gyugyu@pepabo.com>
2022-04-22 16:03:17 +0900

    bump ts and add test for command

--- test/FontSpec.test.ts
+++ test/FontSpec.test.ts
@@ -3,6 +3,6 @@ import { loadFontSpec } from '../src/FontSpec'
 describe('loadFontSpec', () => {
   it('can parse yaml file', () => {
     const spec = loadFontSpec('test/fixtures/regular')
-    expect(spec.name).toStrictEqual('myicons')
+    expect(spec.name).toEqual('myicons')
   })
 })
