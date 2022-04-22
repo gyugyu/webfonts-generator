606047203f95b12538dc8808c84bf30c167d7707
Yusuke Yagyu (gyugyu) <gyugyu@pepabo.com>
2022-04-22 16:13:07 +0900

    bump font generators

--- src/types/svgicons2svgfont.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'svgicons2svgfont' {
-  import fs from 'fs'
-
-  class SVGIcons2SVGFontStream extends fs.WriteStream {
-    constructor(options: Record<string, unknown>)
-  }
-
-  export default SVGIcons2SVGFontStream
-}
